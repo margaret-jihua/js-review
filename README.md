8daf36d6d2e1cf4e58d7ff43936cac40a2c7a84c
margaret-jihua <margaret.jihua@gmail.com>
2020-07-20 14:13:00 -0700

    [js] Add data types

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# JavaScrip Review
+# JavaScript Review
 This is my review of JavaScript with README
+
+## Example of Data Type
+```javascript
+let name = 'Mars'
+let age = 23
+let isCool = true 
+const friends = ['Amy', 'lucy', 'zoey']
+const tesla = {
+    industry: 'Electric Cars',
+    ceo: 'Elon Musk',
+    yearFounded: 2003,
+    foundingMembers: ['Elon Musk', 'JB straubel', 'Martin Eberard'],
+    stockTicker: 'TESLA',
+    vehicals: {
+        vehicalOne: 'Model S',
+        vehicalTwo: 'Model X',
+        vehicalThree: 'Model 3'
+    },
+    print: function(){
+        console.log('Hello, this works with objects');
+    }
+}
+```
---
+++ app.js
@@ -0,0 +1,31 @@
+// Data Tpyes
+
+let name = 'Mars'
+let age = 23
+let isCool = true 
+const friends = ['Amy', 'lucy', 'zoey']
+
+const tesla = {
+    industry: 'Electric Cars',
+    ceo: 'Elon Musk',
+    yearFounded: 2003,
+    foundingMembers: ['Elon Musk', 'JB straubel', 'Martin Eberard'],
+    stockTicker: 'TESLA',
+    vehicals: {
+        vehicalOne: 'Model S',
+        vehicalTwo: 'Model X',
+        vehicalThree: 'Model 3'
+    },
+    print: function(){
+        console.log('Hello, the ceo of Tasla is ' + this.ceo);
+    }
+}
+
+console.log(name);
+console.log(age);
+console.log(isCool);
+console.log(friends);
+console.log(tesla);
+console.log(tesla.vehicals.vehicalThree);
+console.log(tesla['vehicals']['vehicalThree']);
+tesla.print();
